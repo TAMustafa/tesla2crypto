692cadce78139258a63604aabd69f24472460bdb
TAMustafa <tareksnews@gmail.com>
2020-12-31 18:07:51 +0100

    API endpoint string corrected from EUR to USD

--- src/components/Crypto.svelte
+++ src/components/Crypto.svelte
@@ -10,7 +10,7 @@
 
   onMount(async () => {
     const res = await fetch(
-      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1`
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1`
     );
     data = await res.json();
 
